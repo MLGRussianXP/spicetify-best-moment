1a971a09eb0431d99d76cf2cbf3502d5d48619f9
dkqz <daniilkoles74@gmail.com>
2024-08-27 22:40:21 +0500

    Setup ESLint

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ async function main() {
   while (!Spicetify?.showNotification) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
-  
+
   // Show message on start.
   Spicetify.showNotification("Hello!");
 }
